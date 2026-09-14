3b6f8d1f900dcea5b0f168c61e7bee1ac59d330c
vikashtiwari242007-glitch <vikashtiwari242007@gmail.com>
2026-09-14 16:43:48 +0530

    incomplete work

--- musicLibrary.py
+++
@@ -1,7 +0,0 @@
-music ={
-    "stealth": "https://www.youtube.com/watch?v=U47Tr9BB_wE&list=RDU47Tr9BB_wE&start_radio=1",
-    "faded": "https://www.youtube.com/watch?v=qdpXxGPqW-Y",        
-    "alone": "https://www.youtube.com/watch?v=bPs0xFd4skY&list=RDbPs0xFd4skY&start_radio=1",
-    "sugar": "https://www.youtube.com/watch?v=N1BcpzPGlYQ&list=RDN1BcpzPGlYQ&start_radio=1",
-    "skyfall": "https://www.youtube.com/watch?v=sZrTJesvJeo&list=RDsZrTJesvJeo&start_radio=1" 
-}
